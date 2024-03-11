37dbf5e2627df43bcb686158388b265c070ae3dd
Preenaphans <145903958+Preenaphans@users.noreply.github.com>
2024-03-12 02:03:45 +0700

    Update Currency.js

--- Currency.js
+++ Currency.js
@@ -1,10 +1,10 @@
 function toUSD(bath){
-    let value = bath*35.4310
+    let value = bath*0.02823
     return value.toFixed(2) + "ดอลลาร์สหรัฐ"
 }
 
-function toTHB(bath) {
-    let value = bath*0.02822
+function toTHB(usd) {
+    let value = usd*35.435
     return value.toFixed(2) + "บาท"
 }
 
